e336e9ce4b98641f14d776c2ce25a7af3dfdaef9
milouzevich <milouzevich@gmx.fr>
2017-09-15 22:12:33 +0200

    mettre dans téléchargement

--- Linux/install_tools/smarty_3.1.30.sh
+++ Linux/install_tools/smarty_3.1.30.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Installation du template PHP Smarty 3.1.30 sur un système linux
-
+cd Téléchargements/
 wget https://github.com/smarty-php/smarty/archive/v3.1.30.tar.gz
+tar zxvf v3.1.30.tar.gz
