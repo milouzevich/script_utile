36af65d0b95fea66aa5823ce796db434f97002e0
milouzevich <woitteqy@localhost.localdomain>
2017-09-15 15:41:04 +0200

    script add_on_vbox.sh

---
+++ linux/add_on_vbx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Il faut lancer le bouton add-on dans vbox avant de lancer le script
+
+apt-get update
+apt-get upgrade
+apt-get install linux-headers-$(uname -r)
+apt-get install build-essential module-assistant
+
+mount /media/cdrom
+cd /media/cdrom
+sh VboxLinuxAdditions.run
+
+reboot
