d9cf4dbf11aae6bb038d4287c670236025caf4bc
milouzevich <milouzevich@gmx.fr>
2017-09-15 23:01:20 +0200

    commentaire utile

--- Linux/install_tools/smarty_3.1.30.sh
+++ Linux/install_tools/smarty_3.1.30.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 # Installation du template PHP Smarty 3.1.30 sur un système linux
+# Il faut au prealable donne les droits a+x sur le fichier /var/www/*
+# il faut donner le droit à l'utilisateur user:user sur le fichier /var/www/*
+
 cd
 cd Téléchargements/
 wget https://github.com/smarty-php/smarty/archive/v3.1.30.tar.gz
