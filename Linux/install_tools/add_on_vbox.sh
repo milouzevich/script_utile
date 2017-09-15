04be3112d48ff7a2e9b6a3c6e8fb1b7fb95815d8
milouzevich <milouzevich@gmx.fr>
2017-09-15 20:46:33 +0200

    preparation du script des add_on de vbox

---
+++ Linux/install_tools/add_on_vbox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# script permettant d'installer les add_on de virtualbox sur un système linux
+
+echo '[+] Mise a jour du système'
+apt-get update
+apt-get upgrade
+echo '\n\t[+]Mise a jour terminé...'
