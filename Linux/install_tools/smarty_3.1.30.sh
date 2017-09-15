269940687127eaae99d8c837f31391cd9ddbdbe4
milouzevich <milouzevich@gmx.fr>
2017-09-15 22:42:55 +0200

    copie dans env web

--- Linux/install_tools/smarty_3.1.30.sh
+++ Linux/install_tools/smarty_3.1.30.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Installation du template PHP Smarty 3.1.30 sur un système linux
-cd 
+cd
 cd Téléchargements/
 wget https://github.com/smarty-php/smarty/archive/v3.1.30.tar.gz
 tar zxvf v3.1.30.tar.gz
+cd smarty-3.1.30/
+cp -R libs/ /var/www/html/Smarty
+cd
+cd /var/www/html
+mkdir templates templates_c
