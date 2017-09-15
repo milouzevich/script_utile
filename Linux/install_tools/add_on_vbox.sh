eb91ea789d3b25064a8def3be414030667df6f7c
milouzevich <milouzevich@gmx.fr>
2017-09-15 21:24:49 +0200

    test

--- Linux/install_tools/add_on_vbox.sh
+++ Linux/install_tools/add_on_vbox.sh
@@ -5,3 +5,15 @@ echo -e '[+] Mise a jour du système'
 apt-get update
 apt-get upgrade
 echo -e '\n\t[+]Mise a jour terminé...'
+
+echo -e '\n\t[+] Mise place des éléments manquants'
+apt-get install linux-headers-$(uname -r)
+apt-get install build-essential module-assistant
+
+mount /media/cdrom0
+cd /media/cdrom0
+echo -e '\t\t[+] lancement du script add_on Linux'
+sh VBoxLinuxAdditions.run
+
+echo -e '\t\t[+] Le système va être rebooté'
+reboot
